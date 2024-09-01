98dfa58747ce0fb180dcd6266a0cf4297c7b208c
hunya2017 <hunya2017@gmail.com>
2024-09-01 21:41:49 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -9,7 +9,7 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-sed -i '$a src-git openclash https://github.com/vernesong/OpenClash/tree/master/luci-app-openclash' feeds.conf.default
+# sed -i '$a src-git openclash https://github.com/vernesong/OpenClash/tree/master/luci-app-openclash' feeds.conf.default
 
 
 # other
